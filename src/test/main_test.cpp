19e5e4a8e99b805fb8021886c3b4bef2b1965165
bartag84 <bartag84@gmail.com>
2015-12-28 21:01:34 +0100

    Main file for Unit testing is created.

---
+++ src/test/main_test.cpp
@@ -0,0 +1,13 @@
+/*
+ * main_test.cpp
+ *
+ *  Created on: Dec 28, 2015
+ *      Author: gabor
+ */
+
+#include <CppUTest/CommandLineTestRunner.h>
+
+int main(int ac, char** av)
+{
+   return CommandLineTestRunner::RunAllTests(ac, av);
+}
