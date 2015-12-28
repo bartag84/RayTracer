e8f398da51e0166c5ae41c1315e5a2d7f3dd3f59
bartag84 <bartag84@gmail.com>
2015-12-28 21:04:13 +0100

    Doxygen and project tunings

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -1,12 +1,15 @@
-/*
- * main_test.cpp
+/*! \file main_test.cpp
+ *	\brief Main file for Unit testing
  *
- *  Created on: Dec 28, 2015
- *      Author: gabor
+ *	> This file is discarded from the Release build.
+ *
+ *	Created on: Dec 28, 2015\n
+ *	Author: <mailto:bartag84@gmail.com>
  */
 
 #include <CppUTest/CommandLineTestRunner.h>
 
+/*! Unit testing entry point */
 int main(int ac, char** av)
 {
    return CommandLineTestRunner::RunAllTests(ac, av);
