df6f18187a2b5e908e56945f309b72635ac03bf1
bartag84 <bartag84@gmail.com>
2015-12-28 21:18:55 +0100

    Main file for Command Line Application is created. Builds are configured to use the proper main file.

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -1,16 +1,16 @@
 /*! \file main_test.cpp
- *	\brief Main file for Unit testing
- *
- *	> This file is discarded from the Release build.
- *
- *	Created on: Dec 28, 2015\n
- *	Author: <mailto:bartag84@gmail.com>
- */
+ 	\brief Main file for Unit Testing
+
+ 	> This file is discarded from the Release build.
+
+ 	Created on: Dec 28, 2015\n
+ 	Author: <mailto:bartag84@gmail.com>
+*/
 
 #include <CppUTest/CommandLineTestRunner.h>
 
-/*! Unit testing entry point */
-int main(int ac, char** av)
+/*! Unit Testing entry point */
+int main(int argc, char** argv)
 {
-   return CommandLineTestRunner::RunAllTests(ac, av);
+   return CommandLineTestRunner::RunAllTests(argc, argv);
 }
